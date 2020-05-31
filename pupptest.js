1481ba4c570743360ff89b894310a39c6366daf3
theligod <theligod1@gmail.com>
2020-05-30 23:57:05 -0700

    Got both dropdowns to work 11:57PM 5/30/2020

--- pupptest.js
+++ pupptest.js
@@ -9,9 +9,10 @@ async function initiate() {
     });
     const page = await browser.newPage();
 
-    await page.goto('https://www.supremenewyork.com/shop/accessories/w4xp3nrj2/mt26hz7la')
+    await page.goto('https://www.supremenewyork.com/shop/accessories/w4xp3nrj2/mt26hz7la');
     page.waitForNavigation({ waitUntil: 'networkidle0'});
 
-    await page.select('select#s','75628')
+    await page.select('select#s','75628');
+    await page.select('select#qty', '6');
 }
     initiate();
